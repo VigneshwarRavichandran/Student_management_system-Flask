49194208bfb08c92004c34d8daa94fb4a48f89d4
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-05 23:42:44 +0530

    added staff and admin login
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- app.py
+++ app.py
@@ -4,15 +4,37 @@ app = Flask(__name__)
 
 @app.route('/student_login', methods=['POST', 'GET'])
 def student_login():
-	error = message
-  if request.method == 'POST':
-  	data = request.get_json()
-  	print(data)
-    if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-        error = 'Invalid Credentials. Please try again.'
-    else:
-        return redirect(url_for('student'))
-  return render_template('login.html', message=error)
+	message = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			message = 'Invalid Credentials. Please try again.'
+		else:
+			return True
+			# return redirect(url_for('student'))
+	return render_template('login.html', login='Student Login', message=message)
+
+@app.route('/staff_login', methods=['POST', 'GET'])
+def staff_login():
+	message = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			message = 'Invalid Credentials. Please try again.'
+		else:
+			return True
+			# return redirect(url_for('student'))
+	return render_template('login.html', login='Staff Login', message=message)
+
+@app.route('/admin_login', methods=['POST', 'GET'])
+def admin_login():
+	message = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			message = 'Invalid Credentials. Please try again.'
+		else:
+			return True
+			# return redirect(url_for('student'))
+	return render_template('login.html', login='Admin Login', message=message)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
