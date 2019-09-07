4c744a311057e562bdcadde3ef3acceefde4e083
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-07 16:57:35 +0530

    Login Changes
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request
 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1998@localhost/student_management'
 
-@app.route('/student_login', methods=['POST', 'GET'])
-def student_login():
+db = SQLAlchemy(app)
+from models import User
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
 	message = None
 	if request.method == 'POST':
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
@@ -11,29 +16,7 @@ def student_login():
 		else:
 			return True
 			# return redirect(url_for('student'))
-	return render_template('login.html', login='Student Login', message=message)
-
-@app.route('/staff_login', methods=['POST', 'GET'])
-def staff_login():
-	message = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			message = 'Invalid Credentials. Please try again.'
-		else:
-			return True
-			# return redirect(url_for('student'))
-	return render_template('login.html', login='Staff Login', message=message)
-
-@app.route('/admin_login', methods=['POST', 'GET'])
-def admin_login():
-	message = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			message = 'Invalid Credentials. Please try again.'
-		else:
-			return True
-			# return redirect(url_for('student'))
-	return render_template('login.html', login='Admin Login', message=message)
+	return render_template('login.html', message=message)
 
 
 if __name__ == '__main__':
---
+++ models/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1998@localhost/student_management'
+
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True)
+	password = db.Column(db.String(80))
+
+class Staff(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True)
+	password = db.Column(db.String(80))
+
+class admin(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True)
+	password = db.Column(db.String(80))
