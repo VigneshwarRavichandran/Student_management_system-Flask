88c06d63075a59c767e453719533ecdf3ed797c5
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-07 22:56:05 +0530

    Revert Back
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1998@localhost/student_management'
 
 db = SQLAlchemy(app)
-from models import User
+
+class Temp(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -17,7 +20,6 @@ def login():
 			return True
 			# return redirect(url_for('student'))
 	return render_template('login.html', message=message)
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1998@localhost/student_management'
-
-db = SQLAlchemy(app)
-
-class Student(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True)
-	password = db.Column(db.String(80))
-
-class Staff(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True)
-	password = db.Column(db.String(80))
-
-class admin(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True)
-	password = db.Column(db.String(80))
+class Temp(db.Model):
+	id = self.db.Column(self.db.Integer, primary_key=True)
+	username = self.db.Column(self.db.String(80), unique=True)
