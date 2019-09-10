549cb29dc5f0ba3d5b7bf87e06751fd1745e4682
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-10 21:47:19 +0530

    Login works
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- helper/__init__.py
+++ helper/__init__.py
@@ -10,13 +10,10 @@ def get_details(userid):
 	user = session.query(UserDetails).filter_by(user_id=userid).first()
 	user_details = {  
 	  'user_role' : user.user_role,
+	  'user_name' : user.user_name,
 	  'subject_teacher' : user.subject_teacher,
 	  'class_teacher' : user.class_teacher, 
 	  'student_mark' : user.student_mark, 
 	  'student_class' : user.student_class,
-
 	}
-	if user.user_role == 'student':
-		user_details['student_class_teacher'] = session.query(UserDetails.class_teacher).filter_by(class_teacher=user.student_class).first()
-		print(session.query(UserDetails.class_teacher).filter_by(class_teacher=user.student_class).first())
 	return user_details
