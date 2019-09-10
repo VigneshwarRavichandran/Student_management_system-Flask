d53d013bd2767219fd7c74f950be7eff16f3da2f
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-10 13:21:00 +0530

    Login works
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from models import TempUsers
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1998@localhost/student_management'
-
-db = SQLAlchemy(app)
-
-class Temp(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
 	message = None
 	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			message = 'Invalid Credentials. Please try again.'
-		else:
-			return True
+		data = request.form
+		username = data['username']
+		# if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+		# 	message = 'Invalid Credentials. Please try again.'
+		# else:
+		# 	return True
 			# return redirect(url_for('student'))
 	return render_template('login.html', message=message)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,24 @@
-class Temp(db.Model):
-	id = self.db.Column(self.db.Integer, primary_key=True)
-	username = self.db.Column(self.db.String(80), unique=True)
+from sqlalchemy import create_engine, Table, Column, Integer, ForeignKey, String, Text, DateTime
+from sqlalchemy.orm import relationship, Session
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine("mysql://root:1998@localhost/student_management",echo = True)
+Base = declarative_base()
+session = Session(engine)
+
+class Users(Base):
+  __tablename__ = 'users'
+  id = Column(Integer, primary_key=True)
+  username = Column(String(50))
+  password = Column(String(50))
+  details = relationship("UserDetails")
+
+class UserDetails(Base):
+  __tablename__ = 'user_details'
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  user_role = Column(String(50))
+  subject_teacher = Column(String(50))
+  class_teacher = Column(String(50), unique=True)
+  student_mark = Column(Integer)
+  student_class = Column(String(50))
