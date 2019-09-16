4fa756eb058e71993519106858239b8a0ae5c3bd
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-09-17 00:13:09 +0530

    Added comments
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- app.py
+++ app.py
@@ -10,14 +10,18 @@ def register():
 	username = data['username']
 	password = data['password']
 	user_role = data['role']
+	# Password encryption
 	hashed_password = encrypt_password(password)
 	user_exist = get_check(userid)
+	# Check for user existence
 	if not user_exist:
 		if user_role == 'professor':
 			user_subjectid = data['subject_id']
+			# Creating a user with professor role
 			create_professor(userid, username, hashed_password, user_subjectid)
 		elif user_role == 'student':
 			user_departmentid = data['department_id']
+			# Creating a user with student role
 			create_student(userid, username, hashed_password, user_departmentid)
 		return jsonify({
 		'message' : 'User registered successfully'
@@ -32,9 +36,12 @@ def login():
 	userid = data['userid']
 	password = data['password']
 	user = get_user(userid)
+	# Check for user existence
 	if user:
 		hashed_password = user.encrypted_data
+		# Verifing the password by decrypting it
 		if verify_user(password, hashed_password):
+			# Obtaining a access token
 			token = get_token(userid)
 			return jsonify({
 				'token' : token
@@ -54,18 +61,24 @@ def profile():
 		data = request.get_json()
 		action = data['action']
 		response = None
+		# Professor's action to get all his/her students
 		if action == 'get all students':
 			professor = get_professor(userid)
+			# Verify whether the user is professor
 			if not professor:
 				return jsonify({'message' : 'ERROR: Unauthorized user'}), 401
 			response = get_all_students(professor)
+		# Students's action to get all his/her subjects
 		elif action == 'get all subjects':
 			student = get_student(userid)
+			# Verify whether the user is student
 			if not student:
 				return jsonify({'message' : 'ERROR: Unauthorized user'}), 401
 			response = get_all_subjects(student)
+		# Students's action to get all his/her professors
 		elif action == 'get all professors':
 			student = get_student(userid)
+			# Verify whether the user is student
 			if not student:
 				return jsonify({'message' : 'ERROR: Unauthorized user'}), 401
 			response = get_all_professors(student)
